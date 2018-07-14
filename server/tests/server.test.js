a769a1a10a7c41544a83e251e64966c8197b622b
Robert Sraka <robert.sraka@me.com>
2018-07-15 00:58:53 +0200

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,20 @@ const request = require('supertest');
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
+// seed data for the tests
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}];
+
 beforeEach((done) => {
   // se pokliče pred vsakim testom - izbrišem celo bazo todojev
-  Todo.remove({}).then(() => done());
-})
+  Todo.remove({}).then(() => {
+    // nato naložim todoje, ki jih imam za seed data
+    Todo.insertMany(todos);
+  }).then(() => done());
+});
 
 describe('POST /todos', () => {
   it ('should create a new todo', (done) => { // tole je asinhrono, zato moraš počakati na todo
@@ -24,7 +34,7 @@ describe('POST /todos', () => {
         if (err) {
           return done(err);
         }
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -42,9 +52,21 @@ describe('POST /todos', () => {
           return done(err);
         }
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((e) => done(e));
       });
-  })
+  });
 });
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  })
+})
