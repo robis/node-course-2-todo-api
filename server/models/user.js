685c9096d96005170e6c83475dbcbff1eedece76
Robert Sraka <robert.sraka@me.com>
2018-07-15 17:15:10 +0200

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,67 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+// kaj bomo potrebovali:
+// {
+//   email: 'robert@sraka.org';
+//   password: 'fhoiwue98236982t3y9832f2',
+//   tokens: [{
+//     access: 'auth',
+//     token: 'oiwheoiuhosiehgoiehgesg'
+//   }]
+// }
+
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     minlength: 1,
-    trim: true
-  }
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email.'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
+
+// overrinding a method (da samo tisto pošljemo nazaj, kar je treba)
+UserSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+}
+
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'kvaka998!').toString();
+
+  user.tokens = user.tokens.concat([{access, token}]);
+
+  return user.save().then(() => {
+    return token;
+  })
+}
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ require('./config/config.js');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
+const { ObjectID } = require('mongodb');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -32,7 +32,7 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({todos});
+    res.send({ todos });
   }, (e) => {
     res.status(400).send(e);
   })
@@ -47,16 +47,16 @@ app.get('/todos/:id', (req, res) => {
     return res.status(404).send();
   }
   // findById
-    // succes
-      // if todo - send it back
-      // if no todo - send back 404 with empty body
-    // error
-      // 400 - send
+  // succes
+  // if todo - send it back
+  // if no todo - send back 404 with empty body
+  // error
+  // 400 - send
   Todo.findById(id).then((todo) => {
     if (!todo) {
       return res.status(404).send();
     }
-    res.send({todo});
+    res.send({ todo });
   }).catch((e) => res.status(400).send());
 })
 
@@ -72,7 +72,7 @@ app.delete('/todos/:id', (req, res) => {
     if (!todo) {
       return res.status(404).send();
     }
-    res.send({todo});
+    res.send({ todo });
   }).catch((e) => res.status(400).send());
 })
 
@@ -91,18 +91,32 @@ app.patch('/todos/:id', (req, res) => {
     body.completedAt = null;
   }
 
-  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
     if (!todo) {
       return res.status(404).send();
     }
-    res.send({todo});
+    res.send({ todo });
   }).catch((e) => {
     res.status(400).send();
   })
 })
 
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
 
-module.exports = {app};
+module.exports = { app };
